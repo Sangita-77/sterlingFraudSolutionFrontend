837542d6ba83bc9d0becce04136ed70cf439c02b
Sangita Singh <sangita@dreamlogodesign.com>
2026-05-01 14:49:42 +0530

    change on config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/dev/sterlingFraudSolutionFrontend/",
+  base: "/dev/sterlingFraudSolutionFrontend",
   plugins: [react()],
   server: {
     host: true,      // important (exposes to network)
